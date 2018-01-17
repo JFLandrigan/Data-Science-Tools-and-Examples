915e5d80c86eb5e3210db3f3e3d83ca2de07393f
JFLandrigan <jon.landrigan@gmail.com>
2018-01-17 11:14:26 -0500

    Create get_user_tweets.py

---
+++ Misc/get_user_tweets.py
@@ -0,0 +1,20 @@
+def get_user_tweets(username = "", cnt = 200, ckey = "", csecret = "", akey = "", asecret = ""):
+    #This function grabs tweets from a specific user 
+    #requires tweepy and pandas
+    
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(ckey, csecret)
+    auth.set_access_token(akey, asecret)
+    api = tweepy.API(auth)
+    
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    tweets = api.user_timeline(screen_name = username, count = cnt)
+    
+    #initiate empty dataframe
+    tweetData = pd.DataFrame(columns=['username', 'date', 'text'])
+    #loop through the tweets and populate the dataframe
+    for i in range(len(tweets)):
+        tweetData.loc[i] = [username, tweets[i].created_at, tweets[i].text]
+    
+    #return the tweet dataframe
+    return(tweetData)
