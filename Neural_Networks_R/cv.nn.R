5b0eceeeb554a9669e0e4381e38a200073042ed4
JFLandrigan <jon.landrigan@gmail.com>
2018-02-06 21:06:50 -0500

    Update cv.nn.R

--- Neural_Networks_R/cv.nn.R
+++ Neural_Networks_R/cv.nn.R
@@ -1,32 +1,44 @@
-cv.nn <- function(dat = NA, inVars = NA, outVars = NA, hidLayers = NA, foldCol = NA, numFolds = 10, 
+cv.nn <- function(dat = NA, inVars = NA, output = NA, hidLayers = NA, numFolds = 10, 
                   learn_rate = .01, lin_out = FALSE, thresh = .01, steps = 100000, alg = 'rprop+'){
   
-  #Function to run k-fold cross validation with neuralnet
   #The function requires the caret and neuralnet packages to be used.
+  #dat should be a dataframe containing all the input and output data
+  #inVars should be a vector containing the names of the columns for the input variables
+  #output arg should be the name of the column containing the output values
+  #hidLayers expects a vector containg the numerical values for the number of units in each hidden layer 
   #The alg argument can accept any of the algorithms supported by neuralnet()
-  #outVars and inVars should be vectors containing the names of inputs (inVars) and the output(s) (outVars)
-  #For classification neuralnet() expects the outcome var to be coded using the one hot encoding method
-  #foldCol arg should be single column that contains the true outputs 
   
   #load required packages
   require(caret)
   require(neuralnet)
-  require(plyr)
+  
+  if(lin_out == FALSE){
+    #generate the one hot encoding matrix
+    outMat <- model.matrix(~ 0 + dat[,output], dat)
+    #change the names of the outMat and store the names of the outcome variables
+    colnames(outMat) <- unique(dat[,output])
+    outnames <- colnames(outMat)
+    
+    #bind the one hot encoding matrix to the dat
+    dat <- cbind(dat, outMat)
+  }else{
+    outnames <- output
+    }
   
   #set up the model formula
-  mod.formula <- as.formula(paste(paste(outVars, collapse = "+"), "~", paste(inVars, collapse = " + ")))
+  mod.formula <- as.formula(paste(paste(outnames, collapse = "+"), "~", paste(inVars, collapse = " + ")))
   
   #If classification set up the missClassifcation matrix else keep as NA
   if(lin_out == FALSE){
     #store misclass info
-    numClasses <- length(unique(foldCol))
+    numClasses <- length(unique(dat[,output]))
     missClass <- array(data = rep(0, numClasses^2), dim = c(numClasses, numClasses))
   }else{
     missClass <- NA
   }
   
   #get the testing indeces using the createFolds function provided by the caret package
-  folds <- createFolds(foldCol, k = numFolds)
+  folds <- createFolds(dat[,output], k = numFolds)
   
   #results is a vector that will contain the accuracy for each of the network trainings and testing
   results <- c()
@@ -43,17 +55,17 @@ cv.nn <- function(dat = NA, inVars = NA, outVars = NA, hidLayers = NA, foldCol =
     
     if(lin_out){
       #Calc RMSE
-      results <- c(results, sqrt(mean((preds$net.result - dat[fld, outVars])^2)))
+      results <- c(results, sqrt(mean((preds$net.result - dat[fld, outnames])^2)))
   
     }else{
       #Calc the mean classification accuracy
       classRes <- preds$net.result
       nnClass <- apply(classRes, MARGIN = 1, which.max)
-      origClass <- apply(dat[fld , outVars], MARGIN = 1, which.max)  
+      origClass <- apply(dat[fld , outnames], MARGIN = 1, which.max)  
       results <- c(results, mean(nnClass == origClass) * 100)
       
       #Update the missclass matrix
-      for(class in 1:length(outVars)){
+      for(class in 1:length(outnames)){
         #get the indeces for where the origClass was predicted in the fold by nn
         inds <- which(origClass == class)
         #get the count info for number of classifications of each type by nn
@@ -66,8 +78,8 @@ cv.nn <- function(dat = NA, inVars = NA, outVars = NA, hidLayers = NA, foldCol =
   
   #if classification add column and row names to the miss class matrix
   if(lin_out == FALSE){
-    rownames(missClass) <- outVars
-    colnames(missClass) <- outVars
+    rownames(missClass) <- outnames
+    colnames(missClass) <- outnames
   }
   
   #Train the network on the entire dataset
