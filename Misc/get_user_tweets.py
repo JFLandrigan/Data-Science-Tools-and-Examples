8e8d67294293384b1a183e30363a64cc5c016ea4
JFLandrigan <jon.landrigan@gmail.com>
2018-02-06 21:35:24 -0500

    Update get_user_tweets.py

--- Misc/get_user_tweets.py
+++ Misc/get_user_tweets.py
@@ -1,5 +1,5 @@
 def get_user_tweets(username = "", cnt = 200, ckey = "", csecret = "", akey = "", asecret = ""):
-    #This function grabs tweets from a specific user 
+    #This function grabs tweets from a specific user and stores the tweets in a pandas dataframe
     #requires tweepy and pandas
     
     #authorize twitter, initialize tweepy
