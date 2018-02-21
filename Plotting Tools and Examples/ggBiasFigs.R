4148500320ff8793d10c4c838da731ad931dd772
JFLandrigan <jon.landrigan@gmail.com>
2018-02-21 15:22:19 -0500

    Update ggBiasFigs.R

--- Plotting Tools and Examples/ggBiasFigs.R
+++ Plotting Tools and Examples/ggBiasFigs.R
@@ -1,4 +1,4 @@
-ggBiasFigs <- function(dat = NA, grid = TRUE, bar = TRUE){
+ggBiasFigs <- function(dat = NA, grid = TRUE, bar = TRUE,  sumFig = TRUE){
   
   #ggBiasFig is a function to make basic figures to display bias information
   #dat - expects a dataframe in wide format with a column called Study containing the study labels and
@@ -21,17 +21,18 @@ ggBiasFigs <- function(dat = NA, grid = TRUE, bar = TRUE){
   
   if(grid){
   
-    gr <- ggplot(longDat, aes(Study, variable, fill = value)) + 
+    gr <- ggplot(longDat, aes(Study, variable, fill = as.character(value))) + 
       geom_tile(aes(width = 0.95, height = 0.95)) + 
       #set colors
-      scale_fill_gradient2(low = "red", mid = "yellow", high = "green", midpoint = 0) +
+      scale_fill_manual(name = "Bias",
+                        values = c("-1" = "red", "0" = "yellow", "1" = "green"), 
+                        labels = c("high", "uncertain", "low")) +
       #black and white theme with set font size
       theme_bw(base_size = 10) + 
       #rotate x-axis labels so they don't overlap, get rid of unnecessary axis titles, adjust plot margins
       theme(axis.text.x = element_text(angle = 90), 
             axis.title.x = element_blank(), 
-            axis.title.y = element_blank()) +
-      guides(fill = F)
+            axis.title.y = element_blank()) # + guides(fill = F)
   
   }else{gr <- NA}
   
@@ -40,22 +41,44 @@ ggBiasFigs <- function(dat = NA, grid = TRUE, bar = TRUE){
     #reverse the order of the studies for plot
     longDat$Study <- reverse.levels(longDat$Study)
     
+    scaleFact <- length(unique(longDat$Study))
+    
     #-1 = red, 0 = green, 1 = blue ---ggplot default
     
     br <- ggplot(longDat, aes(Study, fill = as.character(value))) +
       geom_bar(aes( y = ((..count..)/sum(..count..))*20 )) + 
       scale_y_continuous(labels = scales::percent) +
-      scale_fill_manual(values = c("-1" = "red", "0" = "yellow", "1" = "green")) +
+      scale_fill_manual(name = "Bias",
+                        values = c("-1" = "red", "0" = "yellow", "1" = "green"), 
+                        labels = c("high", "uncertain", "low")) +
       #flip the coordinates so studies along y
       coord_flip() +
       theme_bw() +
       theme(axis.text.x = element_text(angle = 90), 
             axis.title.x = element_blank(), 
-            axis.title.y = element_blank()) +
-      guides(fill = F)
+            axis.title.y = element_blank()) #+ guides(fill = T)
     
   }else{br <- NA}
   
-  return(list(grid = gr, percBars = br))
+  if(sumFig){
+    
+    scaleFact <- length(unique(longDat$variable))
+    
+    sf <- ggplot(longDat, aes(variable, fill = as.character(value))) +
+      geom_bar(aes( y = ((..count..)/sum(..count..))*7 )) + 
+      scale_y_continuous(labels = scales::percent) +
+      scale_fill_manual(name = "Bias",
+                        values = c("-1" = "red", "0" = "yellow", "1" = "green"), 
+                        labels = c("high", "uncertain", "low")) +
+      #flip the coordinates so studies along y
+      coord_flip() +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 90), 
+            axis.title.x = element_blank(), 
+            axis.title.y = element_blank())
+    
+  }else{sf < NA}
+  
+  return(list(grid = gr, percBars = br, biasSummary = sf))
   
 }
