36acede68960642148b52f53c6c68df7de41d689
JFLandrigan <jon.landrigan@gmail.com>
2020-05-31 21:58:31 -0400

    Add files via upload

--- Model-Tuner/setup.py
+++ Model-Tuner/setup.py
@@ -4,7 +4,7 @@ setup(
     name="Model_Tuner",
     version="0.0.1",
     description="Machine Learning Package to help practitioners develop models",
-    url="",
+    url="https://github.com/JFLandrigan/Data-Science-Tools-and-Examples/tree/master/Model-Tuner",
     author="Jon-Frederick Landrigan",
     author_email="jon.landrigan@gmail.com",
     packages=find_packages(),
